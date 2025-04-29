8286cd6c9868fa950e942d8485473061405175d3
Isabella Calvo <114623758+Bellacalvo@users.noreply.github.com>
2025-04-29 10:08:30 -0700

    Update copy.ino
    UPDATED CODE!
    -Instead of controlling a solenoid valve, it is now controlling the water pump
    -We give the PIR motion sensor 30 seconds to calibrate before detecting motion
    -Motion is always detected, but the water pump will turn on for 5 seconds once triggered. During those 5 seconds, the motion sensor will not be detected.

--- copy.ino
+++ copy.ino
@@ -1,40 +1,50 @@
-#define LED_1 3
 #define motionSensorPin 2
-#define valvePin 4
+#define relayPin 4
 
-bool trigger = false;
+bool triggered = false;
+unsigned long triggerTime = 0;
+const unsigned long blockDuration = 5000; // 5 seconds
+const int calibrationTime = 30; // seconds
 
 void setup() {
-  Serial.begin(9600); 
-  pinMode(LED_1, OUTPUT);
   pinMode(motionSensorPin, INPUT);
-  pinMode(valvePin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(9600); 
 
-  digitalWrite(LED_1, LOW);      
-  digitalWrite(valvePin, LOW);   
+  digitalWrite(relayPin, LOW);  
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
 }
 
 void loop() {
   bool motionDetected = digitalRead(motionSensorPin) == HIGH;
 
-  Serial.print("Motion Detected: ");
-  Serial.println(motionDetected ? "Yes" : "No");
-
-  if (motionDetected) {
-    if (!trigger) {
-      digitalWrite(valvePin, HIGH);
-      delay(10000) #delay for to seconds so the valve will run
-      digitalWrite(LED_1, HIGH);
-      Serial.println("Motion has been detected! Light and Valve ON");
-      trigger = true;
-    }
-  } else {
-    digitalWrite(valvePin, LOW);
-    digitalWrite(LED_1, LOW);
-    trigger = false;
+  if (!triggered && motionDetected) {
+    // Only trigger if not already triggered
+    triggered = true;
+    triggerTime = millis();
+    digitalWrite(relayPin, HIGH); // Turn ON pump
+    Serial.println("Motion detected! Pump ON");
+  }
+  else if (!triggered && !motionDetected) {
+    // No motion and pump is not running
+    Serial.println("No motion detected.");
   }
 
-  delay(500); 
+  if (triggered && (millis() - triggerTime >= blockDuration)) {
+    // After 15 seconds, turn OFF pump and reset trigger
+    digitalWrite(relayPin, LOW); // Turn OFF pump
+    Serial.println("5 seconds passed. Pump OFF");
+    triggered = false;
+  }
+
+  delay(100);
 }
 
 
