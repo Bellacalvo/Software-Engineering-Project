e8aefdb6302d9312fd8ab162885b255a7a1f08c5
Isabella Calvo <114623758+Bellacalvo@users.noreply.github.com>
2025-04-30 10:18:14 -0700

    Update software_project.ino
    rename variables to be more clear

--- software_project.ino
+++ software_project.ino
@@ -3,9 +3,9 @@
 #define relayPin 4 // pin connected to the relay
 
 //state variables
-bool triggered = false; //track to see if the pump has been triggered
-unsigned long triggerTime = 0; //store the time when motion has been detected
-const unsigned long blockDuration = 5000; // time for how long the pump will be on (5 seconds)
+bool pump_triggered = false; //track to see if the pump has been triggered
+unsigned long pump_trigger_time = 0; //store the time when motion has been detected
+const unsigned long pumpDuration = 5000; // time for how long the pump will be on (5 seconds)
 const int calibrationTime = 30; // time for how long the motion sesnor will calibrate
 
 //On startup it will initialize the pins for input and output
@@ -22,8 +22,7 @@ void setup() {
       Serial.print(".");
       delay(1000);
       }
-    Serial.println(" done");
-    Serial.println("SENSOR ACTIVE");
+    Serial.println(The sensor is ready");
     delay(50);
 }
 
@@ -31,23 +30,23 @@ void setup() {
 void loop() {
   bool motionDetected = digitalRead(motionSensorPin) == HIGH;
   // motion is detected and the pump has not been triggered yet
-  if (!triggered && motionDetected) {
+  if (!pump_triggered && motionDetected) {
     // Only trigger if not already triggered
-    triggered = true;
-    triggerTime = millis();
+    pump_triggered = true;
+    pump_trigger_time = millis();
     digitalWrite(relayPin, HIGH); // Turn Pump ON
     Serial.println("Motion detected! Pump ON");
   }
   // no motion and pump is not ON
-  else if (!triggered && !motionDetected) {
+  else if (!pump_triggered && !motionDetected) {
     // No motion and pump is not running
     Serial.println("No motion detected.");
   }
   //Pump is running and the duration of 5 seconds has passed
-  if (triggered && (millis() - triggerTime >= blockDuration)) {
+  if (pump_triggered && (millis() - pump_trigger_time >= pumpDuration)) {
     digitalWrite(relayPin, LOW); // Turn pump OFF
     Serial.println("5 seconds passed. Pump OFF");
-    triggered = false;
+    pump_triggered = false;
   }
 
   delay(100); // Small delay
